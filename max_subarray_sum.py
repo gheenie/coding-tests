0570fe672000f5c78ba7a3ba3e6bcd2366496afa
infoomics <sbx_94@hotmail.com>
2023-06-13 23:27:04 +0100

    add alt solution based on i

--- max_subarray_sum.py
+++ max_subarray_sum.py
@@ -1,24 +1,42 @@
 def max_sequence(arr):
     max_sum = 0
     
-    for subarray_length in range(1, len(arr) + 1):
-        current_sum = sum(arr[:subarray_length])
-        
-        if current_sum > max_sum:
-            max_sum = current_sum
-        
-        for i in range(len(arr) - subarray_length):
-            current_sum -= arr[i]
-            current_sum += arr[i + subarray_length]
+    for i in range(len(arr)):
+        j = i
+        current_sum = 0
+
+        while j != len(arr):
+            current_sum += arr[j]
             
             if current_sum > max_sum:
                 max_sum = current_sum
+            
+            j += 1
     
     return max_sum
 
 
 print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
 # Should be 6: [4, -1, 2, 1]
+
+
+# def max_sequence(arr):
+#     max_sum = 0
+    
+#     for subarray_length in range(1, len(arr) + 1):
+#         current_sum = sum(arr[:subarray_length])
+        
+#         if current_sum > max_sum:
+#             max_sum = current_sum
+        
+#         for i in range(len(arr) - subarray_length):
+#             current_sum -= arr[i]
+#             current_sum += arr[i + subarray_length]
+            
+#             if current_sum > max_sum:
+#                 max_sum = current_sum
+    
+#     return max_sum
 
 
 # def max_sequence(arr):
