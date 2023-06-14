90aedbe43cd9ca98c27f5e64f5e43ac96ea85dfb
infoomics <sbx_94@hotmail.com>
2023-06-14 13:23:40 +0100

    Implement Kadane's algo

--- max_subarray_sum.py
+++ max_subarray_sum.py
@@ -1,94 +1,15 @@
 def max_sequence(arr):
-    # stepped_additions = [0]
-    
-    # for element in arr:
-    #     stepped_additions.append(stepped_additions[-1] + element)
-
+    best_sum = 0
     current_sum = 0
-    max_sum = 0
-    index_max_sum = -1
-    for i, element in enumerate(arr):
-        current_sum += element
-        if current_sum > max_sum:
-            max_sum = current_sum
-            index_max_sum = i
-
-    if index_max_sum == -1:
-        return 0
-
-    current_sum = max_sum
-    for i in range(index_max_sum + 1):
-        current_sum -= arr[i]
-        if current_sum > max_sum:
-            max_sum = current_sum
     
-    return max_sum
+    for x in arr:
+        # Reset the front pointer of the subarray whenever its sum goes negative
+        # to the next positive num
+        current_sum = max(0, current_sum + x)
+        best_sum = max(best_sum, current_sum)
+    
+    return best_sum
 
 
 print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
 # Should be 6: [4, -1, 2, 1]
-
-
-# def max_sequence(arr):
-#     max_sum = 0
-    
-#     for subarray_length in range(1, len(arr) + 1):
-#         current_sum = sum(arr[:subarray_length])
-        
-#         if current_sum > max_sum:
-#             max_sum = current_sum
-        
-#         for i in range(len(arr) - subarray_length):
-#             current_sum -= arr[i]
-#             current_sum += arr[i + subarray_length]
-            
-#             if current_sum > max_sum:
-#                 max_sum = current_sum
-    
-#     return max_sum
-
-
-# def max_sequence(arr):
-#     max_sum = 0
-
-#     subarray_length = 1
-#     current_sum = 0
-#     i = 0
-#     is_traversing_right = True
-
-#     while subarray_length != len(arr) + 1:
-#         if is_traversing_right:
-#             current_sum += arr[subarray_length - 1]
-#         else:
-#             current_sum += arr[i - subarray_length + 1]
-
-#         if current_sum > max_sum:
-#             max_sum = current_sum
-
-#         if is_traversing_right:
-#             while i != len(arr) - subarray_length:
-#                 current_sum -= arr[i]
-#                 current_sum += arr[i + subarray_length]
-
-#                 if current_sum > max_sum:
-#                     max_sum = current_sum
-
-#                 i += 1
-
-#             i = len(arr) - 1
-#         else:
-#             while i != subarray_length - 1:
-#                 current_sum -= arr[i]
-#                 current_sum += arr[i - subarray_length]
-
-#                 if current_sum > max_sum:
-#                     max_sum = current_sum
-
-#                 i -= 1
-
-#             i = 0
-
-#         is_traversing_right = not is_traversing_right
-#         subarray_length += 1
-    
-#     return max_sum
